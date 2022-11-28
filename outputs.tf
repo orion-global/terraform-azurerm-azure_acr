46c6045e11383c8be93a0dbb6b1e557009481456
Rodolfo Castelo Mendez <rodolfo.castelo@outlook.com>
2022-11-28 16:06:32 +0100

    Add registry credentials
    Signed-off-by: Rodolfo Castelo Mendez <rodolfo.castelo@outlook.com>

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,11 @@ output "registry_id" {
 output "registry_name" {
   value = azurerm_container_registry.registry.login_server
 }
+
+output "admin_username" {
+  value = azurerm_container_registry.registry.admin_username
+}
+
+output "admin_password" {
+  value = azurerm_container_registry.registry.admin_password
+}
